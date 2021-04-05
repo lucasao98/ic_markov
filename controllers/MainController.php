873fd6c93c238ec034a8985e72ea1c8223573196
João Henrique <johnrick.jh@gmail.com>
2021-04-05 20:20:30 -0300

    Alterações na visualização dos resultados

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -55,13 +55,14 @@ class MainController extends Controller
                 $states[$i] = 0;
             }
 
-            $three_states = [0, 0, 0];
 
             $cursor_by_price[0]["t_state"] = 2;
 
+            $three_states = [0, 0, 0];
+
             foreach ($cursor_by_price as $index => $cursor) { //atribui um estado a partir do preço de fechamento para cada data no conjunto de treinamento        
-                if($index > 0) {
-                    $cursor['t_state'] = $model->getThreeSate($cursor['preult'], $cursor_by_price[$index-1]['preult']);
+                if ($index > 0) {
+                    $cursor['t_state'] = $model->getThreeSate($cursor['preult'], $cursor_by_price[$index - 1]['preult']);
                 }
 
                 $three_states[$cursor['t_state'] - 1] += 1;
@@ -82,8 +83,8 @@ class MainController extends Controller
                 //'db' => $cursor_by_price,
                 'vector' => $vector,
                 't_vector' => $three_state_vector,
-                'last' => $cursor_by_price[count($cursor_by_price)-1],
-                'last_price' => $cursor_by_price[count($cursor_by_price)-1]['preult'],
+                'last' => $cursor_by_price[count($cursor_by_price) - 1],
+                'last_price' => $cursor_by_price[count($cursor_by_price) - 1]['preult'],
                 'model' => $model,
                 'stock' => $model->nome,
                 'states_number' => $model->states_number,
@@ -145,28 +146,28 @@ class MainController extends Controller
         else
             $result = 'Errou!';
 
-        switch($t_max) {
-            case 0: 
-                if($nextDay['preult'] > $last_price)
+        switch ($t_max) {
+            case 0:
+                if ($nextDay['preult'] > $last_price)
                     $result_three_state = 'Acertou!';
                 else
                     $result_three_state = 'Errou!';
                 break;
 
-            case 1: 
-                if($nextDay['preult'] == $last_price)
+            case 1:
+                if ($nextDay['preult'] == $last_price)
                     $result_three_state = 'Acertou!';
                 else
                     $result_three_state = 'Errou!';
                 break;
 
-            case 2: 
-                if($nextDay['preult'] < $last_price)
+            case 2:
+                if ($nextDay['preult'] < $last_price)
                     $result_three_state = 'Acertou!';
                 else
                     $result_three_state = 'Errou!';
                 break;
-            
+
             default:
                 break;
         }
@@ -197,10 +198,14 @@ class MainController extends Controller
             $consultas = 0;
             $acertou = 0;
             $errou = 0;
+            $t_acertou = 0;
+            $t_errou = 0;
             $next = array();
             $intervals = array();
             $aux = Paper::toIsoDate(\DateTime::createFromFormat('d/m/YH:i:s', $model->final . '24:00:00')->format('U'));
             $nextDay = new Paper();
+            // $last_prices = array();
+            // $next_prices = array();
 
             $final = $start;
             $start = \DateTime::createFromFormat('d/m/YH:i:s', $start . '24:00:00'); //Dia de início do conjunto de treinamento
@@ -213,7 +218,7 @@ class MainController extends Controller
 
             $stock = $model->nome;
             $cursor_by_price = $model->PegarDados($stock, $start, $final); //Setup inicial do conjunto de treinamento
-            
+
             $predictStart = \DateTime::createFromFormat('d/m/YH:i:s', $model->inicio . '24:00:00');
             $nextDays = $model->PegarDados($stock, Paper::toIsoDate($predictStart->format('U')), $aux); //Busca no banco os dias que serão previstos
             $consultas = count($nextDays);
@@ -221,21 +226,16 @@ class MainController extends Controller
 
             while (1) {
 
-                if(count($nextDays) == 0)
+                if (count($nextDays) == 0)
                     break;
 
                 $nextDay = $nextDays[0]; //busca o dia seguinte no banco
                 array_shift($nextDays);
-                // $date = $nextDay['date']->toDateTime()->format('d/m/Y');
-                // print($date."\n");
-                // $last = $cursor_by_price[count($cursor_by_price)-1]['date']->toDateTime()->format('d/m/Y');
-                // print($last."\n");
-                // print("Ultima: ".$last."Proxima: ".$date."\n");
-                
+
                 //Se o dia a ser previsto for maior do que o nosso ultimo dia estipulado o laço ou nulo acaba
                 if ($nextDay['date'] > $aux || $nextDay['date'] == null)
                     break;
-  
+
                 $premin = $model->DefinirPremin($cursor_by_price);
                 $premax = $model->DefinirPremax($cursor_by_price);
 
@@ -246,15 +246,26 @@ class MainController extends Controller
                     $states[$i] = 0;
                 }
 
-                
-                foreach ($cursor_by_price as $cursor) { //atribui um estado a partir do preço de fechamento para cada data no conjunto de treinamento
-                        $cursor['state'] = $model->getState($cursor['preult'], $premin['preult'], $interval, $model->states_number);
-                        if ($cursor['state'] != 0)
-                            $states[$cursor['state'] - 1] += 1;
+                $cursor_by_price[0]["t_state"] = 2;
+
+                $three_states = [0, 0, 0];
+
+                foreach ($cursor_by_price as $index => $cursor) { //atribui um estado a partir do preço de fechamento para cada data no conjunto de treinamento
+                    if ($index > 0) {
+                        $cursor['t_state'] = $model->getThreeSate($cursor['preult'], $cursor_by_price[$index - 1]['preult']);
+                    }
+
+                    $three_states[$cursor['t_state'] - 1] += 1;
+
+                    $cursor['state'] = $model->getState($cursor['preult'], $premin['preult'], $interval, $model->states_number);
+                    if ($cursor['state'] != 0)
+                        $states[$cursor['state'] - 1] += 1;
                 }
 
-                $matrix = $model->transitionMatrix($cursor_by_price, $states, $model->states_number, "state"); //função que constrói a matriz de transição
+                $three_state_matrix = $model->transitionMatrix($cursor_by_price, $three_states, 3, "t_state");
+                $three_state_vector = $model->predictVector($three_state_matrix, $cursor_by_price, 3, "t_state"); //função que constrói o vetor de predição
 
+                $matrix = $model->transitionMatrix($cursor_by_price, $states, $model->states_number, "state"); //função que constrói a matriz de transição
                 $vector = $model->predictVector($matrix, $cursor_by_price, $model->states_number, "state"); //função que constrói o vetor de predição
                 /* Validação ----------------------------------------------------------------- */
 
@@ -262,11 +273,18 @@ class MainController extends Controller
 
                 array_push($next, $nextDay);
                 $max = 0;
+                $t_max = 0;
                 $vector = $vector[0];
+                $t_vector = $three_state_vector[0];
 
                 for ($i = 1; $i < $model->states_number; $i++) { //calculando o estado com maior probabilidade no vetor de previsão
                     if ($vector[$i] >= $vector[$max])
                         $max = $i;
+                }
+
+                for ($i = 1; $i < 3; $i++) { //calculando o estado com maior probabilidade no vetor de previsão
+                    if ($t_vector[$i] >= $t_vector[$t_max])
+                        $t_max = $i;
                 }
 
                 array_push($intervals, $model->getInterval($premin['preult'], $interval, $max));
@@ -274,27 +292,40 @@ class MainController extends Controller
                     $acertou++;
                 else
                     $errou++;
+
+                $last_price =  $cursor_by_price[count($cursor_by_price) - 1]['preult'];
+                // $last_date =  intval(($cursor_by_price[count($cursor_by_price) - 1]['date']->toDateTime())->format('U') . '000');
+                // $next_date =  intval(($nextDay['date']->toDateTime())->format('U') . '000');
+
+                // array_push($next_prices, [$next_date, $nextDay['preult']]);
+                // array_push($last_prices, [$last_date, $last_price]);
+
+                switch ($t_max) {
+                    case 0:
+                        if ($nextDay['preult'] > $last_price)
+                            $t_acertou++;
+                        else
+                            $t_errou++;
+                        break;
+
+                    case 1:
+                        if ($nextDay['preult'] == $last_price)
+                            $t_acertou++;
+                        else
+                            $t_errou++;
+                        break;
+
+                    case 2:
+                        if ($nextDay['preult'] < $last_price)
+                            $t_acertou++;
+                        else
+                            $t_errou++;
+                        break;
+
+                    default:
+                        break;
+                }
                 /* Preparação para a próxima iteração ----------------------------------------------------------------- */
-
-                //Se o dia previsto foi um mais a frente que o próximo o programa continua a partir do dia previsto
-                // if ($nextDay['date'] != Paper::toIsoDate(($final->toDateTime()->modify('+1 day')->format('U')))) {
-                //     $nextAux = $nextDay['date']->toDateTime()->modify("-$model->periodo month");
-                //     $start = Paper::toIsoDate($nextAux->getTimestamp());
-                // }
-
-                // $start = $start->toDateTime()->modify("+$model->periodo month");
-                // $start = $start->modify('+1 day');
-                // $start = $start->format('d/m/Y');
-                // $consultas++;
-
-                // $final = $start;
-                // $start = \DateTime::createFromFormat('d/m/YH:i:s', $start . '24:00:00'); //Dia de início do conjunto de treinamento
-                // $start = $start->modify("-$model->periodo month"); //O conjunto de treinamento será definido n meses antes do dia a ser previsto
-                // /* -------------------------------------------------------------------- */
-                // $final = \DateTime::createFromFormat('d/m/YH:i:s', $final . '24:00:00')->modify('-1 day'); //Dia final do conjunto de treinamento
-
-                // $start = Paper::toIsoDate($start->format('U')); //Passando para o padrão de datas do banco
-                // $final = Paper::toIsoDate($final->format('U')); //Passando para o padrão de datas do banco
 
                 array_shift($cursor_by_price);
                 array_push($cursor_by_price, $nextDay);
@@ -306,12 +337,16 @@ class MainController extends Controller
             return $this->render('resultadoTeste', [
                 'acertou' => $acertou,
                 'errou' => $errou,
+                't_acertou' => $t_acertou,
+                't_errou' => $t_errou,
                 'consultas' => $consultas,
                 'fechamentoData' => $chart['fechamentoData'],
                 'avgData' => $chart['avgData'],
                 'supData' => $chart['supData'],
                 'infData' => $chart['infData'],
                 'tendencia' => $chart['tendencia']
+                // 'last_prices' => $last_prices,
+                // 'next_prices' => $next_prices
                 // 'next' => $next,
                 // 'intervals' => $intervals
             ]);
--- views/main/resultadoTeste.php
+++ views/main/resultadoTeste.php
@@ -14,8 +14,14 @@
 
 <body>
     <!-- Resultado -->
+    <h1>Previsão com estados fixos</h1>
     <h3 class="result-acertou"><?= "Acertou: $acertou - " . round(($acertou / $consultas) * 100, 2) . '%' ?></h3>
     <h3 class="result-errou"><?= "Errou: $errou - " . round(($errou / $consultas) * 100, 2) . '%' ?></h3>
+    <br />
+    <h1>Previsão com 3 estados</h1>
+    <h3 class="result-acertou"><?= "Acertou: $t_acertou - " . round(($t_acertou / $consultas) * 100, 2) . '%' ?></h3>
+    <h3 class="result-errou"><?= "Errou: $t_errou - " . round(($t_errou / $consultas) * 100, 2) . '%' ?></h3>
+    <br />
     <h3><?= "Acompanhou a tendência: $tendencia vezes - " . round(($tendencia / $consultas) * 100, 2) . '%' ?></h3>
 
     <!-- <h4>
@@ -47,6 +53,17 @@
                 'data' => $infData
             ]
         ];
+
+        // $series3 = [
+        //     [
+        //         'name' => 'Preço do dia anterior',
+        //         'data' => $last_prices
+        //     ],
+        //     [
+        //         'name' => 'Preço do dia seguinte',
+        //         'data' => $next_prices
+        //     ]
+        // ];
 
         $series2 = [
             [
@@ -113,6 +130,61 @@
             ],
             'series' => $series
         ]);
+
+        // echo \onmotion\apexcharts\ApexchartsWidget::widget([
+        //     'type' => 'line', // default area
+        //     'height' => '400', // default 350
+        //     'width' => '1200', // default 100%
+        //     'chartOptions' => [
+        //         'markers' => [
+        //             'size' => 4
+        //         ],
+        //         'chart' => [
+        //             'toolbar' => [
+        //                 'show' => true,
+        //                 'autoSelected' => 'zoom'
+        //             ],
+        //         ],
+        //         'xaxis' => [
+        //             'type' => 'datetime',
+        //             'hideOverlappingLabels' => false,
+        //             'datetimeFormatter' => [
+        //                 'day' => 'dd MMM yyyy'
+        //             ]
+        //             // 'categories' => $categories,
+        //         ],
+        //         'yaxis' => [
+        //             'title' => [
+        //                 'text' => 'Preço (R$)'
+        //             ]
+        //         ],
+        //         'plotOptions' => [
+        //             'bar' => [
+        //                 'horizontal' => false,
+        //                 'endingShape' => 'rounded'
+        //             ],
+        //         ],
+        //         'dataLabels' => [
+        //             'enabled' => false
+        //         ],
+        //         'stroke' => [
+        //             'show' => true,
+        //             'makers' => ['size' => 1]
+        //         ],
+        //         'legend' => [
+        //             'verticalAlign' => 'bottom',
+        //             'horizontalAlign' => 'center',
+        //             'labels' => [
+        //                 'useSeruesColors' => true
+        //             ]
+        //         ],
+        //         'makers' => [
+        //             'size' => 0
+        //         ],
+        //         'colors' => ['#ffff00', '#0000cc']
+        //     ],
+        //     'series' => $series3
+        // ]);
 
         echo \onmotion\apexcharts\ApexchartsWidget::widget([
             'type' => 'line', // default area

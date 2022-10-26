9f5eba6673c4621ebfc0d873bdfa86e7a35f2742
lucasao98 <lucasacramento32@gmail.com>
2022-10-25 23:26:09 -0300

    Mensagem de resultado do estado estável mudou

--- views/main/steady-state-result.php
+++ views/main/steady-state-result.php
@@ -7,7 +7,7 @@
 
     <h2>Resultado do Cálculo do Estado Estável</h2>
 
-    <h3>A Matriz convergiu com após <?=  $times ?> iterações com valores de <?= $matrix ?>.</h3>
+    <h3>A Matriz convergiu com após <?=  $times ?> dias com valores de <?= $matrix ?>.</h3>
     <div class="form-group mt-2">
         <div class="col-lg-offset-0">
             <a class="btn btn-warning" href="steady-state-predict">Voltar</a>
